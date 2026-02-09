27b48e3a2cd8ec2895952a6c08fa966e853d5e61
Nyamosii <ian.obino@strathmore.edu>
2026-02-09 12:23:36 +0300

    Add project files.

---
+++ Data/Worker.cs
@@ -0,0 +1,20 @@
+﻿namespace Blazor_Training.Data
+{
+    public class Worker
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public double hourlyPay { get; set; }
+        public int hoursWorked { get; set; }
+        public double totalSalary => hourlyPay * hoursWorked;
+
+        public Worker() { }
+
+        public Worker(string _name, double _hourlypay, int _hoursworked)
+        {
+            Name = _name;
+            hourlyPay = _hourlypay;
+            hoursWorked = _hoursworked;
+        }
+    }
+}
---
+++ Data/WorkerService.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+namespace Blazor_Training.Data
+{
+    public class WorkerService
+    {
+        private static List<Worker> myWorkers = new List<Worker>();
+
+        public List<Worker> GetAllWorkers()
+        {
+            return myWorkers;
+        }
+
+        public void AddWorker(Worker worker)
+        {
+            myWorkers.Add(worker);
+        }
+        public Worker GetWorker(Guid Id)
+        {
+            return myWorkers.FirstOrDefault(w => w.Id == Id);
+        }
+        public void DeleteWorker(Worker worker)
+        {
+            myWorkers.Remove(worker);
+        }
+    }
+}
